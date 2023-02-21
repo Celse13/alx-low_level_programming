bc5bf55f24ff8e4ab39b165c9e25f2fbe8295a1e
Celse13 <rugiracelse13@gmail.com>
2023-02-21 14:17:04 -0800

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,39 @@
 #include "main.h"
+
+
+
 /**
-* is_lowerChecker
-* Return: (0)success -if lowercase,(1) if failed.
+
+* main - check the code.
+
+*
+
+* Return: Always 0.
+
 */
 
+int main(void)
+
+{
+
+int r;
 
 
-int _islower(int c)
-{
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
+
+r = _islower('H');
+
+_putchar(r + '0');
+
+r = _islower('o');
+
+_putchar(r + '0');
+
+r = _islower(108);
+
+_putchar(r + '0');
+
+_putchar('\n');
+
 return (0);
-}
-}
 
+}
