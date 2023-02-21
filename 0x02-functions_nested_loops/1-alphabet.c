4f7fea5566dc562245c407a8e5a7153a3446f54e
Celse13 <rugiracelse13@gmail.com>
2023-02-21 12:48:36 -0800

    Third commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabets in lowercase.
+ * Return: On success 1.
+ */
+void print_alphabet(void)
+{
+        char i;
+
+        for (i = 'a'; i <= 'z'; i++)
+        {
+            putchar(i);
+        }
+        putchar('\n');
+}
