3a4a8dedd9a5f402c50c0140c67688ff08f5539c
Celse13 <rugiracelse13@gmail.com>
2023-02-21 14:34:42 -0800

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,16 +4,14 @@
 * run _isalpha-check.
 * Return:1-if lowercase/uppercase,0-if not.
 */
+int _isalpha(int c) {
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+return 1;
+}
+else {
+return 0;
+}
+}
 
-int _isalpha(int c)
-{
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
-return (1);
-}
-else
-{
-return (0);
-}
-}
+
 
