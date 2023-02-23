691311a148af138c267f9b730b94f569f2aa3f5e
Celse13 <rugiracelse13@gmail.com>
2023-02-23 11:00:17 -0800

    Forth commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - prints a message to the console
 * Return: 0
 */
 
 int main(void)
 {
-int i;
-
 printf("Infinite loop incoming :(\n");
-
-i = 0;
-
-// while (i < 10)
-// {
-//     putchar(i);
-// }
 
 printf("Infinite loop avoided! \\o/\n");
 
