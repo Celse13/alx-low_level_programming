515af4d3b562d9d6ebf2fc527a4b72ea29fb1e8e
Celse13 <rugiracelse13@gmail.com>
2023-02-23 17:50:08 -0800

    C commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,12 @@
 * @c: int to be checked
 * Return: 1 if c is a digit, 0 otherwise
 */
-int _isdigit(int c)
-{
-return (c >= '0' && c <= '9');
+
+int _isdigit(int c) {
+if (c >= '0' && c <= '9') {
+return 1;
+} else {
+return 0;
 }
+}
+
