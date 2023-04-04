ed80605957fe628ce7479cccc7351ebcd0d706fd
Celse13 <rugiracelse13@gmail.com>
2023-04-04 09:22:21 -0700

    list.h

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -7,7 +7,7 @@
 /**
 * struct listint_s - singly linked list
 * @n: integer
-* @next: points to the next node*
+* @next: points to the next node *
 * Description: singly linked list node structure
 * for alx project
 */
@@ -32,6 +32,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
 #endif
+
 
