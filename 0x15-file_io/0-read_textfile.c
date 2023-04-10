e1e5b9dace7510f6fd6346b864773becf7a2e052
Celse13 <rugiracelse13@gmail.com>
2023-04-10 05:05:12 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,47 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
-* read_textfile- Read text file print to STDOUT.
-* @filename: text file being read
-* @letters: number of letters to be read
-* Return: w- actual number of bytes read and printed
-* 0 when function fails or filename is NULL.
-*/
-ssize_t read_textfile(const char *filename, size_t letters)
+ * main - check the code
+ *Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t max_bytes)
 {
 char *buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
+ssize_t fd, n_read, n_written, total_written;
+
+if (!filename)
+return (0);
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-buf = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
+
+buf = malloc(sizeof(char) * max_bytes);
+if (!buf)
+{
+close(fd);
+return (0);
+}
+
+total_written = 0;
+while ((n_read = read(fd, buf, max_bytes)) > 0)
+{
+n_written = write(STDOUT_FILENO, buf, n_read);
+if (n_written != n_read)
+{
+	free(buf);
+	close(fd);
+	return (0);
+}
+total_written += n_written;
+}
 
 free(buf);
 close(fd);
-return (w);
+
+if (n_read == -1 || total_written == 0)
+return (0);
+
+return (total_written);
 }
 
