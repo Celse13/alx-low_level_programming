64d12abeb9886777bbbe06e782e266b89ac2e0f4
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-05-04 09:36:03 +0300

    Create Header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+unsigned int flip_bits(unsigned long int n1, unsigned long int n2);
+int clear_bit(unsigned long int *num_ptr, unsigned int bit_index);
+int set_bit(unsigned long int *num_ptr, unsigned int bit_index);
+int get_bit(unsigned long int num, unsigned int idx);
+int get_endianness(void);
+void print_binary(unsigned long int num);
+unsigned int binary_to_uint(const char *binary);
+#endif
