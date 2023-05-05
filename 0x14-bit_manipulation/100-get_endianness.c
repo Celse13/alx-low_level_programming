4f91a66a08b76294b8e1de720a877814fc1731db
celse13 <rugiracelse13@gmail.com>
2023-05-05 13:34:51 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,9 +2,8 @@
 
 /**
 * get_endianness - checks if a machine is little or big endian
-*
-* This function checks whether the machine it is run on is little or big endian.
-*
+* This function checks whether the machine
+* it is run on is little or big endian.
 * Return: 0 if the machine is big endian, 1 if it is little endian
 */
 int get_endianness(void)
@@ -12,6 +11,10 @@ int get_endianness(void)
 unsigned int test_val = 1;
 char *test_ptr = (char *) &test_val;
 
-return (*test_ptr);
+if (*test_ptr == 1)
+{
+return (1);
+}
+return (0);
 }
 
