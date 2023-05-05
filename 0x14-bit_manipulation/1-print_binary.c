0cfa6d0375babf9a87678f18faea8baa6004cb9c
celse13 <rugiracelse13@gmail.com>
2023-05-05 12:44:27 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,45 +1,30 @@
 #include "main.h"
 
 /**
-* print_binary - prints the binary representation of a number
-* @num: the number to print in binary
+* print_binary - Prints the binary representation of a num
+* @num: The number to be printed
+*
+* Return: nothing
 */
-void print_binary(unsigned long int num)
+void print_binary(unsigned long int num);
 {
-int i, num_digits = 0;
-unsigned long int mask;
-/* Find the number of binary digits in num */
-for (i = 63; i >= 0; i--)
-{
-mask = num >> i;
-if (mask & 1)
-{
-num_digits++;
-}
-}
-/* Print the binary digits from left to right */
-for (i = 63; i >= 0; i--)
-{
-mask = num >> i;
-if (mask & 1)
-{
-_putchar('1');
-}
-else
-{
-if (num_digits > 1)
-{
-_putchar('0');
-}
-}
-if (mask & 1)
-{
-num_digits--;
-}
-}
 if (num == 0)
 {
 _putchar('0');
+return;
+}
+unsigned int mask = 1;
+unsigned int bit_count = sizeof(unsigned long int) * 8;
+
+while (bit_count--)
+{
+unsigned int msb = n >> bit_count;
+
+if (msb & mask)
+_putchar('1');
+else
+_putchar('0');
 }
 }
+
 
