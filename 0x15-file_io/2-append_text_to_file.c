e50df947092191c2f1a4a290185ae3426efdb39d
celse13 <rugiracelse13@gmail.com>
2023-05-08 13:43:51 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
 *
 * Return: On success, returns 1. On failure, returns -1.
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int file_descriptor, length = 0;
 if (filename == NULL)
