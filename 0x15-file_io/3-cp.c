37a069276ae8d99708e4393dd9337456169ae57f
celse13 <rugiracelse13@gmail.com>
2023-05-08 15:12:18 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,54 +1,6 @@
 #include "main.h"
-/**
-* open_files - Open source and destination files.
-* @src_file: Pointer to the source file name.
-* @dest_file: Pointer to the destination file name.
-* Return: Returns the file descriptor of the source file.
-*/
-int open_files(const char *src_file, const char *dest_file)
-{
-int src_fd, dest_fd;
-src_fd = open(src_file, O_RDONLY);
-if (src_fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", src_file);
-exit(2);
-}
-dest_fd = open(dest_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
-S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (dest_fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Unable to write to %s\n", dest_file);
-close(src_fd);
-exit(3);
-}
-return (src_fd);
-}
-/**
-* copy_files - Copy content from source file to destination file.
-* @src_fd: File descriptor of the source file.
-* @dest_fd: File descriptor of the destination file.
-*/
-void copy_files(int src_fd, int dest_fd)
-{
-char buffer[BUFFER_SIZE];
-int read_count, write_count;
+#define BUFFER_SIZE 1024
 
-while ((read_count = read(src_fd, buffer, BUFFER_SIZE)) > 0)
-{
-write_count = write(dest_fd, buffer, read_count);
-if (write_count < read_count)
-{
-dprintf(STDERR_FILENO, "Error: Unable to write to dest_file\n");
-exit(3);
-}
-}
-if (read_count == -1)
-{
-dprintf(STDERR_FILENO, "Error: Unable to read from source file\n");
-exit(2);
-}
-}
 /**
 * custom_copy - Customized version of the 'cp' command to copy file content.
 * @arg_count: Number of arguments passed to the program.
@@ -58,29 +10,51 @@ exit(2);
 */
 int custom_copy(int arg_count, char *arg_values[])
 {
-int src_fd, dest_fd;
+int src_file, dest_file;
+int read_count = BUFFER_SIZE, write_count = 0;
+char buffer[BUFFER_SIZE];
 if (arg_count != 3)
 {
-dprintf(STDERR_FILENO, "Invalid usage: custom_cp src_file dest_file\n");
+dprintf(STDERR_FILENO, "Invalid usage: custom_cp src_file dest\n");
 exit(1);
 }
-src_fd = open_files(arg_values[1], arg_values[2]);
-dest_fd = open(arg_values[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (dest_fd == -1)
+src_file = open(arg_values[1], O_RDONLY);
+if (src_file == -1)
+{
+dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", arg_values[1]);
+exit(2);
+}
+dest_file = open(arg_values[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
+S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+if (dest_file == -1)
 {
 dprintf(STDERR_FILENO, "Error: Unable to write to %s\n", arg_values[2]);
-close(src_fd);
+close(src_file);
 exit(3);
 }
-copy_files(src_fd, dest_fd);
-if (close(src_fd) == -1)
+while (read_count == BUFFER_SIZE)
 {
-dprintf(STDERR_FILENO, "Error: Unable to close source file descriptor\n");
+read_count = read(src_file, buffer, BUFFER_SIZE);
+if (read_count == -1)
+{
+dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", arg_values[1]);
+exit(2);
+}
+write_count = write(dest_file, buffer, read_count);
+if (write_count < read_count)
+{
+dprintf(STDERR_FILENO, "Error: Unable to write to %s\n", arg_values[2]);
+exit(3);
+}
+}
+if (close(src_file) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Unable to close fd %d\n", src_file);
 exit(4);
 }
-if (close(dest_fd) == -1)
+if (close(dest_file) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Unable to close destination file descriptor\n");
+dprintf(STDERR_FILENO, "Error: Unable to close fd %d\n", dest_file);
 exit(4);
 }
 return (0);
