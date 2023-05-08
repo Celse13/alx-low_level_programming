af821db22d261afdf75c07cb10ed1bb0662cd077
celse13 <rugiracelse13@gmail.com>
2023-05-08 13:22:32 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,21 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int file_descriptor, write_status, length = 0;
-
+int file_descriptor, length = 0;
 if (filename == NULL)
 return (-1);
 
 if (text_content == NULL)
 text_content = "";
-while (text_content != '\0')
+while (text_content[length] != '\0')
 {
 length++;
 }
 
 file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-write_status = write(file_descriptor, text_content, length);
-
-if (file_descriptor == -1 || write_status == -1)
+if (file_descriptor == -1)
 return (-1);
+write(file_descriptor, text_content, length);
 
 close(file_descriptor);
 
