ef8a0e2adc8f0fa4ea922856fadffc11b884e88e
Celse13 <rugiracelse13@gmail.com>
2023-07-10 21:35:38 -0700

    0x14-bit_manipulation - main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@ unsigned int flip_bits(unsigned long int n1, unsigned long int n2);
 int clear_bit(unsigned long int *num_ptr, unsigned int bit_index);
 int set_bit(unsigned long int *num_ptr, unsigned int bit_index);
 int get_bit(unsigned long int num, unsigned int idx);
+unsigned int binary_to_uint(const char *binary);
 int get_endianness(void);
 void print_binary(unsigned long int num);
-unsigned int binary_to_uint(const char *binary);
 #endif
