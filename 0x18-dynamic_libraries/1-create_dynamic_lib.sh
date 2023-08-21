05cbe7d096bf5b91d0a2f5ca45ef72c131723009
Celse13 <rugiracelse13@gmail.com>
2023-08-21 08:39:32 -0700

    second task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic-fPIC
-gco -shared -o liball.so.0 
-export LD LIBRARY PATH=.:$LD LIBRARY_PATH
+gcc -Wall -Wextra -Werror -pedantic -fPIC -c *.c
+gcc -shared -o liball.so.0 *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
