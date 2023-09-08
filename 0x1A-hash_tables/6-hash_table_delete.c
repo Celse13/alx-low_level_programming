02f82d2c846f4dc23c4ba2efa475b4fd684310a3
Celse13 <rugiracelse13@gmail.com>
2023-09-08 08:28:44 -0700

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
  *  * hash_table_delete - freeing up the hash table;
- *   * @ht: pointer to the first nod of the hash table
+ *   * @ht: pointer to the first nod of the hash
  *   **/
 
 void hash_table_delete(hash_table_t *ht)
@@ -9,6 +9,9 @@ void hash_table_delete(hash_table_t *ht)
 	hash_table_t *hash_table = ht;
 	unsigned long int n = 0;
 	hash_node_t *current, *tempor_node;
+
+	if (ht == NULL)
+		return;
 
 	while (n < hash_table->size)
 	{
