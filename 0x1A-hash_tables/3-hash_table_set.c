d5a97be990c47ab838708010d3afa30920144c55
Celse13 <rugiracelse13@gmail.com>
2023-09-08 06:04:36 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,24 +4,17 @@
  *   * @ht: pointing to the first node of a hash table.
  *    * @key: key to be used to find hash.
  *     * @value: value for the key.
- *      *
- *       * Return: 1 if successful or 0 once it fails.
- *        **/
+ *      * Return: 1 if successful or 0 once it fails.
+ *       **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	char *duplicate_key = NULL;
-	char *duplicate_value = NULL;
-	unsigned long int hash;
-	hash_node_t *current;
-	hash_node_t *prev;
-
+	char *duplicate_key = NULL, *duplicate_value = NULL;
+	unsigned long int hash, n;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	duplicate_key = strdup(key);
-
 	if (duplicate_key == NULL)
 		return (0);
 
@@ -31,47 +24,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(duplicate_key);
 		return (0);
 	}
-
 	hash = key_index((const unsigned char *)key, ht->size);
-
+	n = hash;
+	while (ht->array[n] != NULL)
+	{
+		if (strcmp(ht->array[n]->key, key) == 0)
+		{
+			free(ht->array[n]->value);
+			ht->array[n]->value = duplicate_value;
+			return (1);
+		}
+		n++;
+	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (!new_node)
 	{
 		free(duplicate_key);
 		free(duplicate_value);
 		return (0);
 	}
-
 	new_node->key = duplicate_key;
 	new_node->value = duplicate_value;
-	new_node->next = NULL;
-
-	if (ht->array[hash] == NULL)
-	{
-		ht->array[hash] = new_node;
-	}
-	else
-	{
-		current = ht->array[hash];
-		prev = NULL;
-
-		while (current != NULL)
-		{
-			if (strcmp(current->key, key) == 0)
-			{
-				free(current->value);
-				current->value = duplicate_value;
-
-				free(duplicate_key);
-				free(new_node);
-
-				return (1);
-			}
-			prev = current;
-			current = current->next;
-		}
-
-		prev->next = new_node;
-	}
+	new_node->next = ht->array[hash];
+	ht->array[hash] = new_node;
 	return (1);
 }
