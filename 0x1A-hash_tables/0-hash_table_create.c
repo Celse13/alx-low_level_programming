31a49d8ee10caa5c17b317d986aa01cf0fa9b25f
Celse13 <rugiracelse13@gmail.com>
2023-09-08 03:18:39 -0700

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ *  * hash_table_create - creating a hash table
+ *   * @size: size of new hash table
+ *    * Return: pointer to the new created hash table
+ *    **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *n_hash_t;
+	unsigned int i = 0;
+
+	n_hash_t = malloc(sizeof(hash_table_t));
+
+	if (n_hash_t == NULL)
+		return (NULL);
+	n_hash_t->size = size;
+	n_hash_t->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (n_hash_t->array == NULL)
+	{
+		free(n_hash_t);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		n_hash_t->array[i] = NULL;
+		i++;
+	}
+
+	return (n_hash_t);
+
+}
